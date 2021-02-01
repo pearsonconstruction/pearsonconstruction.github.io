fd81e213d59e0cf67769ca245f6884f1bd14794a
pearsonconstruction <78294450+pearsonconstruction@users.noreply.github.com>
2021-02-01 17:27:37 +1300

    tweak design

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
     devtool: isProd ? undefined : 'source-map',
     devServer: {
         contentBase: './dist',
+        hot: true,
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
